7dda6cc99915e77dedb10f72653b3b40cfc1fb21
plushmonkey <plushmonkey.ss@gmail.com>
2018-02-21 19:55:17 -0500

    Add RaiseEarth wall
    - Refactor RaiseEarth so it works with both column and wall versions.

--- core/src/main/java/com/plushnode/atlacore/game/ability/earth/RaiseEarth.java
+++ core/src/main/java/com/plushnode/atlacore/game/ability/earth/RaiseEarth.java
@@ -11,6 +11,7 @@ import com.plushnode.atlacore.game.Game;
 import com.plushnode.atlacore.game.ability.Ability;
 import com.plushnode.atlacore.game.ability.ActivationMethod;
 import com.plushnode.atlacore.game.ability.UpdateResult;
+import com.plushnode.atlacore.platform.Location;
 import com.plushnode.atlacore.platform.User;
 import com.plushnode.atlacore.platform.block.Block;
 import com.plushnode.atlacore.platform.block.BlockFace;
@@ -18,90 +19,114 @@ import com.plushnode.atlacore.platform.block.Material;
 import com.plushnode.atlacore.util.MaterialUtil;
 import com.plushnode.atlacore.util.VectorUtil;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class RaiseEarth implements Ability {
     public static Config config = new Config();
 
     private User user;
-    private Block base;
-    private int length;
-    private int currentLength;
-    private long startTime;
     private List<Block> affected = new ArrayList<>();
+    private List<Column> columns = new ArrayList<>();
+    private long duration;
+    private long interval;
+    private int maxHeight;
+    private long lastTime;
 
     @Override
     public boolean activate(User user, ActivationMethod method) {
         this.user = user;
+        boolean wall = method == ActivationMethod.Sneak;
 
-        Block selection = RayCaster.blockCast(user.getWorld(), new Ray(user.getEyeLocation(), user.getDirection()), config.selectRange, true);
+        double selectRange;
+        long cooldown;
+        int width = 1;
+
+        if (wall) {
+            cooldown = config.wallCooldown;
+            this.duration = config.wallDuration;
+            this.interval = config.wallInterval;
+            this.maxHeight = config.wallMaxHeight;
+            selectRange = config.wallSelectRange;
+            width = config.wallWidth;
+        } else {
+            cooldown = config.columnCooldown;
+            this.duration = config.columnDuration;
+            this.interval = config.columnInterval;
+            this.maxHeight = config.columnMaxHeight;
+            selectRange = config.columnSelectRange;
+        }
+
+        Block selection = RayCaster.blockCast(user.getWorld(), new Ray(user.getEyeLocation(), user.getDirection()), selectRange, true);
         if (selection == null || !MaterialUtil.isEarthbendable(selection)) {
             return false;
         }
 
-        this.base = getBase(selection);
-        if (this.base == null || !MaterialUtil.isEarthbendable(this.base)) {
+        if (!createCenterColumn(selection)) {
             return false;
         }
 
-        if (isRaisedBlock(this.base)) {
-            return false;
+        if (wall) {
+            addWallColumns(selection, width);
         }
 
-        this.length = getLength();
+        user.setCooldown(this, cooldown);
 
-        if (this.length > 0) {
-            if (!Game.getProtectionSystem().canBuild(user, base.getLocation())) {
-                return false;
-            }
-
-            user.setCooldown(this);
-            return true;
-        }
-
-        return false;
+        return true;
     }
 
-    @Override
-    public UpdateResult update() {
-        long time = System.currentTimeMillis();
+    private boolean createCenterColumn(Block selection) {
+        Block base = getBase(selection);
 
-        if (currentLength >= length) {
-            // Keep it around long enough for the TempBlocks to time out.
-            // This is so isRaisedBlock can check the affected blocks.
-            if (time >= startTime + config.duration + 1000) {
-                return UpdateResult.Remove;
-            }
-            return UpdateResult.Continue;
+        if (base == null || !MaterialUtil.isEarthbendable(base) || isRaisedBlock(base)) {
+            return false;
         }
 
-        if (time >= startTime + config.interval) {
-            Block top = base.getRelative(BlockFace.UP, currentLength);
-            Block aboveTop = top.getRelative(BlockFace.UP);
-
-            // Move entities into the air when standing on top of the RaiseEarth.
-            AABB collider = AABB.BLOCK_BOUNDS.at(aboveTop.getLocation());
-            CollisionUtil.handleEntityCollisions(user, collider, (entity) -> {
-                entity.setVelocity(VectorUtil.setY(entity.getVelocity(), 1));
-                return false;
-            }, true, true);
-
-            if (!raise(top, length, config.duration)) {
-                currentLength = length;
-            }
-
-            ++currentLength;
-            // Keep restarting startTime for every movement.
-            this.startTime = time;
+        if (!Game.getProtectionSystem().canBuild(user, base.getLocation())) {
+            return false;
         }
 
-        return UpdateResult.Continue;
+        Column column = new Column(base);
+
+        columns.add(column);
+
+        return column.isValid();
+    }
+
+    private void addWallColumns(Block selection, int width) {
+        int firstSideSize = (int)Math.ceil((width - 1) / 2.0);
+        int secondSideSize = (int)Math.floor((width - 1) / 2.0);
+        Vector3D side = user.getDirection().crossProduct(Vector3D.PLUS_J).normalize();
+
+        Location location = selection.getLocation().add(0.5, 0.5, 0.5);
+        for (int i = 0; i < firstSideSize; ++i) {
+            Block check = location.add(side.scalarMultiply(i + 1)).getBlock();
+            Block base = getBase(check);
+
+            if (base == null || !MaterialUtil.isEarthbendable(base) || isRaisedBlock(base)) {
+                continue;
+            }
+
+            columns.add(new Column(base));
+        }
+
+        for (int i = 0; i < secondSideSize; ++i) {
+            Block check = location.add(side.scalarMultiply((i + 1) * -1)).getBlock();
+            Block base = getBase(check);
+
+            if (base == null || !MaterialUtil.isEarthbendable(base) || isRaisedBlock(base)) {
+                continue;
+            }
+
+            columns.add(new Column(base));
+        }
     }
 
     private Block getBase(Block selection) {
-        for (int i = 0; i < config.maxLength; ++i) {
+        for (int i = 0; i < maxHeight; ++i) {
             Block check = selection.getRelative(BlockFace.UP, i);
 
             if (!MaterialUtil.isEarthbendable(check)) {
@@ -116,47 +141,27 @@ public class RaiseEarth implements Ability {
         return null;
     }
 
-    private int getLength() {
-        for (int i = 0; i < config.maxLength; ++i) {
-            Block check = this.base.getRelative(BlockFace.DOWN, i);
+    @Override
+    public UpdateResult update() {
+        long time = System.currentTimeMillis();
 
-            if (!MaterialUtil.isEarthbendable(check)) {
-                return i;
+        if (time > lastTime + interval) {
+            affected.clear();
+
+            for (Iterator<Column> iterator = columns.iterator(); iterator.hasNext(); ) {
+                Column column = iterator.next();
+                if (!column.update()) {
+                    iterator.remove();
+                    continue;
+                }
+
+                affected.addAll(column.affected);
             }
 
-            if (!Game.getProtectionSystem().canBuild(user, check.getLocation())) {
-                return i;
-            }
+            this.lastTime = time;
         }
 
-        return config.maxLength;
-    }
-
-    private boolean raise(Block top, int length, long duration) {
-        affected.clear();
-
-        if (top.getRelative(BlockFace.UP).hasBounds()) {
-            return false;
-        }
-
-        for (int i = 0; i < length; ++i) {
-            Block current = top.getRelative(BlockFace.DOWN, i);
-            Block newBlock = current.getRelative(BlockFace.UP);
-
-            if (!Game.getProtectionSystem().canBuild(user, current.getLocation())) {
-                return false;
-            }
-
-            if (!Game.getProtectionSystem().canBuild(user, newBlock.getLocation())) {
-                return false;
-            }
-
-            affected.add(newBlock);
-            new TempBlock(newBlock, current.getType(), duration);
-        }
-
-        new TempBlock(top.getRelative(BlockFace.DOWN, length - 1), Material.AIR, duration);
-        return true;
+        return columns.isEmpty() ? UpdateResult.Remove : UpdateResult.Continue;
     }
 
     public static boolean isRaisedBlock(Block block) {
@@ -185,24 +190,136 @@ public class RaiseEarth implements Ability {
 
     }
 
+    private class Column {
+        private Block base;
+        private int height;
+        private int currentHeight;
+        private boolean finished;
+        private long lastTime;
+        private List<Block> affected = new ArrayList<>();
+
+        public Column(Block base) {
+            this.base = base;
+            this.currentHeight = 0;
+            this.finished = false;
+
+            this.height = getHeight();
+        }
+
+        public boolean isValid() {
+            return this.height > 0;
+        }
+
+        public boolean update() {
+            long time = System.currentTimeMillis();
+
+            if (finished) {
+                return time < this.lastTime + duration + 1000;
+            }
+
+            affected.clear();
+
+            this.lastTime = time;
+
+            Block top = base.getRelative(BlockFace.UP, currentHeight);
+            Block aboveTop = top.getRelative(BlockFace.UP);
+
+            // Move entities into the air when standing on top of the RaiseEarth.
+            AABB collider = AABB.BLOCK_BOUNDS.at(aboveTop.getLocation());
+            CollisionUtil.handleEntityCollisions(user, collider, (entity) -> {
+                entity.setVelocity(VectorUtil.setY(entity.getVelocity(), 1));
+                return false;
+            }, true, true);
+
+            if (!raise(top, height, duration)) {
+                currentHeight = height;
+                finished = true;
+            }
+
+            if (++currentHeight >= maxHeight) {
+                finished = true;
+            }
+
+            return true;
+        }
+
+        private int getHeight() {
+            for (int i = 0; i < maxHeight; ++i) {
+                Block check = this.base.getRelative(BlockFace.DOWN, i);
+
+                if (!MaterialUtil.isEarthbendable(check) && check.getType() != Material.AIR) {
+                    return i;
+                }
+
+                if (!Game.getProtectionSystem().canBuild(user, check.getLocation())) {
+                    return i;
+                }
+            }
+
+            return maxHeight;
+        }
+
+        private boolean raise(Block top, int height, long duration) {
+            if (top.getRelative(BlockFace.UP).hasBounds()) {
+                return false;
+            }
+
+            for (int i = 0; i < height; ++i) {
+                Block current = top.getRelative(BlockFace.DOWN, i);
+                Block newBlock = current.getRelative(BlockFace.UP);
+
+                if (!Game.getProtectionSystem().canBuild(user, current.getLocation())) {
+                    return false;
+                }
+
+                if (!Game.getProtectionSystem().canBuild(user, newBlock.getLocation())) {
+                    return false;
+                }
+
+                affected.add(newBlock);
+                new TempBlock(newBlock, current.getType(), duration);
+            }
+
+            new TempBlock(top.getRelative(BlockFace.DOWN, height - 1), Material.AIR, duration);
+            return true;
+        }
+    }
+
     private static class Config extends Configurable {
         boolean enabled;
-        long cooldown;
-        long duration;
-        long interval;
-        int maxLength;
-        double selectRange;
+        long columnCooldown;
+        long columnDuration;
+        long columnInterval;
+        int columnMaxHeight;
+        double columnSelectRange;
+        long wallCooldown;
+        long wallDuration;
+        long wallInterval;
+        int wallMaxHeight;
+        double wallSelectRange;
+        int wallWidth;
 
         @Override
         public void onConfigReload() {
             CommentedConfigurationNode abilityNode = config.getNode("abilities", "earth", "raiseearth");
 
             enabled = abilityNode.getNode("enabled").getBoolean(true);
-            cooldown = abilityNode.getNode("cooldown").getLong(500);
-            duration = abilityNode.getNode("duration").getLong(6000);
-            interval = abilityNode.getNode("interval").getLong(100);
-            maxLength = abilityNode.getNode("max-length").getInt(6);
-            selectRange = abilityNode.getNode("select-range").getDouble(8.0);
+
+            CommentedConfigurationNode columnNode = abilityNode.getNode("column");
+
+            columnCooldown = columnNode.getNode("cooldown").getLong(500);
+            columnDuration = columnNode.getNode("duration").getLong(6000);
+            columnInterval = columnNode.getNode("interval").getLong(100);
+            columnMaxHeight = columnNode.getNode("max-height").getInt(6);
+            columnSelectRange = columnNode.getNode("select-range").getDouble(8.0);
+
+            CommentedConfigurationNode wallNode = abilityNode.getNode("wall");
+            wallCooldown = wallNode.getNode("cooldown").getLong(500);
+            wallDuration = wallNode.getNode("duration").getLong(6000);
+            wallInterval = wallNode.getNode("interval").getLong(100);
+            wallMaxHeight = wallNode.getNode("max-height").getInt(6);
+            wallSelectRange = wallNode.getNode("select-range").getDouble(8.0);
+            wallWidth = wallNode.getNode("width").getInt(6);
         }
     }
 }
